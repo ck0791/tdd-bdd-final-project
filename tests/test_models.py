79c14a4e24dbb74ac1633b937366af1b309f6ef0
ck0791 <163730073+ck0791@users.noreply.github.com>
2026-02-16 19:22:14 +0100

    Add test for product availability search
    Added test for finding products by availability.

--- tests/test_models.py
+++ tests/test_models.py
@@ -157,7 +157,7 @@ class TestProductModel(unittest.TestCase):
         products = Product.all()
         self.assertEqual(len(products), 5)
 
-     def test_find_by_name(self):
+    def test_find_by_name(self):
         """It should Find a Product by Name"""
         products = ProductFactory.create_batch(5)
         for product in products:
@@ -166,6 +166,20 @@ class TestProductModel(unittest.TestCase):
         count = len([product for product in products if product.name == name])
         found = Product.find_by_name(name)
         self.assertEqual(found.count(), count)
+
+    def test_find_by_availability(self):
+        """It should Find Products by Availability"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        available = products[0].available
+        count = len([product for product in products if product.available == available])
+        found = Product.find_by_availability(available)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.available, available)
         for product in found:
             self.assertEqual(product.name, name)
+
+
     
