61ad54730d7ed1f8a10e927d0200fd22233b9b8b
ck0791 <163730073+ck0791@users.noreply.github.com>
2026-02-17 11:55:12 +0100

    Implement test for product deletion
    Add a test case for deleting a product in routes.py.

--- service/routes.py
+++ service/routes.py
@@ -146,7 +146,19 @@ def test_update_product(self):
 # D E L E T E   A   P R O D U C T
 ######################################################################
 
-
+def test_delete_product(self):
+    """It should Delete a Product"""
+    products = self._create_products(5)
+    product_count = self.get_product_count()
+    test_product = products[0]
+    response = self.client.delete(f"{BASE_URL}/{test_product.id}")
+    self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    self.assertEqual(len(response.data), 0)
+    # make sure they are deleted
+    response = self.client.get(f"{BASE_URL}/{test_product.id}")
+    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    new_count = self.get_product_count()
+    self.assertEqual(new_count, product_count - 1)
 #
 # PLACE YOUR CODE TO DELETE A PRODUCT HERE
 #
